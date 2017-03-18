4185f124684f4cfb2aa7753418d93a8a92c13941
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-03-18 17:02:17 -0500

    clear selection link added

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next)
-{
-  req.db.collection('flowers').find().toArray(function (err,docs)
-  {
-    if(err)
-    {
-      return next(err)
+router.get('/', function(req, res, next) {
+    req.db.collection('flowers').distinct('color', function (err, colorDocs) {
+        if(err) {
+            return next(err)
+        }
+        if(req.query.color_filter) {
+        req.db.collection('flowers').find({"color":req.query.color_filter}).toArray(function (err, docs) {
+            if (err) {
+                return next(err);
+            }
+            return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs, 'color_filter': req.query.color_filter});
+
+        });
+
     }
-    return res.render('all_flowers', {'flowers': docs} )
+    else {
+        req.db.collection('flowers').find().toArray(function (err, docs) {
+                if (err) {
+                    return next(err)
+                }
+                return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs})
 
-  });
+            });
+        }
 
+    });
 });
 
 module.exports = router;
