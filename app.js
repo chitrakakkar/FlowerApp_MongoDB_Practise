fad51ea571ea4bfeb0a0506fdbb9ebb6fa74ae5e
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-04-13 18:22:09 -0500

    deleted connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var flash = require('connect-flash');
+// var flash = require('connect-flash');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
